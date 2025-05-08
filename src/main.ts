fc28e2beb911f64047c77c5a5dfa52a8f984e8a2
大胃王 <253498229@qq.com>
2025-05-08 15:22:21 +0800

    测试打包发布action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-import * as core from "@actions/core";
-import {info} from "@actions/core";
-import * as github from "@actions/github";
+import * as core from '@actions/core';
+import { info } from '@actions/core';
+import * as github from '@actions/github';
 
 export default function run() {
-    try {
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        info(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        info(`The event payload: ${payload}`);
-    } catch (error: any) {
-        core.setFailed(error.message);
-    }
+  try {
+    core.debug('测试数字1');
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    info(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput('time', time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    info(`The event payload: ${payload}`);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
 }
